88544c2c4d0edcb46b92b76efb6489f04d625940
Jason Lewis <jasonedlewis@gmail.com>
2019-06-22 17:30:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,7 +57,9 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if(cardNumber === ""){
-    return ""
-    
+    return "Sorry, we don't have a credit card on file for you."
+  }else{
+    cart.length = 0;
+    return ``
   }
 }
