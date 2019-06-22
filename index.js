af429728ac499313476f9e4010e7d1ddc90dd1c9
Jason Lewis <jasonedlewis@gmail.com>
2019-06-22 17:26:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,8 +47,6 @@ function removeFromCart(item) {
   for(var i = 0; i < cart.length; i++){
     if(item === cart[i].itemName){
       cart.splice(i,1);
-    }else{
-      return 'That item is not in your cart.'
     }
   }
     return cart;
