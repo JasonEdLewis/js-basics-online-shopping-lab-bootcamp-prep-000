4167f96ac60a51c23d6050bd104145a9cd55cbe6
Jason Lewis <jasonedlewis@gmail.com>
2019-06-22 16:39:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ function viewCart() {
   for (var i = 0; i < cart.length -1 ; i++){
     things.push(`${cart[i].itemName} at $${cart[i].itemPrice}`);
   }
-    let thingsStr = things.join(", ");
+    return things.join(", ");
   }
-  return thingsStr;
+  
   // return `In your cart, you have ${things}, and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}. `;
 }
 
