cac23fbefcfa334c432bb99df294b54f5fd5d868
Jason Lewis <jasonedlewis@gmail.com>
2019-06-22 17:37:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,8 +59,8 @@ function placeOrder(cardNumber) {
   if(cardNumber === undefined){
     return "Sorry, we don't have a credit card on file for you."
   }
-    cart.length = 0;
-    return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
+    
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   
 }
 
